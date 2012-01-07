e7adf37ab1a369d67f38ab9ae9d487ca0b9adf1a
adali <adali@2288d9a7-0fa1-3145-9870-04f04981ec47>
2012-01-07 21:25:42 +0000

    for weighted graph computation, don't assume connections between matched test vertices(nnot allowed to use that information) Comparison of weighted/nonweighted  graphs with minimum weight distance and diffusion distance for matching task
    Former-commit-id: a280461565b046300ebe7b7c0d4766bd8eeb17f1

--- src/JOFC.for.Married.Vertices-fromcep.R
+++ src/JOFC.for.Married.Vertices-fromcep.R
@@ -17,7 +17,12 @@ m = 15 # the first m pairs are known matches ; the last n-m pairs are to-be-matc
 npert = 11
 nmc = 100
 pert=(0:10)/10
-nc.jofc = matrix(0,npert,nmc)
+nc.jofc.diff = matrix(0,npert,nmc)
+nc.jofc.weighted = matrix(0,npert,nmc)
+nc.jofc.unweighted = matrix(0,npert,nmc)
+
+
+
 nc.cmds = matrix(0,npert,nmc)
 matched.cost<-0.01
 
@@ -26,14 +31,9 @@ w.vals<-0.8
 matched.cost<-0.01 #If matched.cost is equal to 1, consider an unweighted graph, with edges between matched vertices
 #If matched.cost is  between 0 and 1, the graph is weighted with edges between matched vertices with weights equal to matched.cost. Edges between 
 # vertices of the same condition have either weight 1 or 2 according to whether they're connected according to given adjacency matrix or not.
-debug.mode<-FALSE
-if (debug.mode){
-	n.v<-100
-	n.np<- 5
-	m<- 5
-}
+
 d.dim<-4
-T.diff<-100
+T.diff<-5
 
 
 # these three lines are not used, really ...
@@ -61,7 +61,7 @@ for(imc in 1:nmc)
 		in.sample.ind[n+oos.sampling]<-FALSE
     
     
-    J =jofc.diffusion.dist(G,Gp,
+    J.1 =jofc.diffusion.dist(G,Gp,
   	in.sample.ind,
 		d.dim=d.dim,
 		graph.is.directed=FALSE,	
@@ -69,8 +69,37 @@ for(imc in 1:nmc)
 		wt.matrix.2=NULL,
 		sep.graphs=TRUE)
 		
-		M = solveMarriage(J)
-		nc.jofc[ipert,imc] = present(M)         # returns the number correct in the marriage?
+		M = solveMarriage(J.1)
+		nc.jofc.diff[ipert,imc] = present(M)         # returns the number correct in the marriage?
+    
+     
+    J.2 =jofc(G,Gp,
+    in.sample.ind,
+		d.dim=d.dim,
+		graph.is.directed=FALSE,	
+		wt.matrix.1=NULL,
+		wt.matrix.2=NULL,
+		sep.graphs=TRUE)
+		
+		M = solveMarriage(J.2)
+		nc.jofc.weighted[ipert,imc] = present(M)         # returns the number correct in the marriage?
+    
+     J.3 =jofc(G,Gp,
+    in.sample.ind,
+  	d.dim=d.dim,
+		graph.is.directed=FALSE,	
+               use.weighted.graph=FALSE,
+		wt.matrix.1=NULL,
+		wt.matrix.2=NULL,
+		sep.graphs=TRUE)
+		
+		M = solveMarriage(J.3)
+		nc.jofc.unweighted[ipert,imc] = present(M)         # returns the number correct in the marriage?
+    
+    
+    
+    
+    
 		if (cep){
 			myD.M = D.M # i use just the object D.M ... none of the original entries!
 			myD.M[1:n,1:n]=D.1
@@ -80,7 +109,7 @@ for(imc in 1:nmc)
 			for(i in 1:n) for(j in (n+m+1):(2*n)) myD.M[i,j] = myD.M[j,i] = 0
 			for(i in (m+1):n) for(j in (n+1):(2*n)) myD.M[i,j] = myD.M[j,i] = 0
 			
-			ccc = cmdscale(myD.M,k=4,eig=T)
+			ccc = cmdscale(myD.M,k=d.dim,eig=T)
 			#plot(ccc$eig)
 			#pairs(ccc$points , col=colvec,pch=c(Ln,Ln))
 			#plot(ccc$points[,c(2,3)] , col=colvec,pch=c(Ln,Ln))
@@ -95,12 +124,18 @@ for(imc in 1:nmc)
 	}
 }
 
-apply(nc.jofc,1,mean)
+
 ### notice that the %correctmatches decreases as perturbation parameter increases! :-)
 
 pdf("plot1.pdf")
-plot(pert,apply(nc.jofc,1,mean)/(n-m),xlab="perturbation parameter",ylab="% correct matches",ylim=c(0,1),col="red")
-points(pert,apply(nc.cmds,1,mean)/(n-m),xlab="perturbation parameter",ylab="% correct matches",ylim=c(0,1),pch=2,col="blue")
+plot(pert,apply(nc.jofc.diff,1,mean)/(n-m),xlab="perturbation parameter",ylab="Fraction of  correct matches",ylim=c(0,1),col="red")
+points(pert,apply(nc.cmds,1,mean)/(n-m),xlab="perturbation parameter",ylab="Fraction of  correct matches",ylim=c(0,1),pch=2,col="blue")
+
+points(pert,apply(nc.jofc.weighted,1,mean)/(n-m),xlab="perturbation parameter",ylab="Fraction of  correct matches",ylim=c(0,1),pch=3,col="orange")
+
+points(pert,apply(nc.jofc.unweighted,1,mean)/(n-m),xlab="perturbation parameter",ylab="Fraction of  correct matches",ylim=c(0,1),pch=4,col="green")
+
+
 #legend("bottomright")
 title("marriage o jofc")
 abline(h=1/(n-m),lty=2) ### chance?  apparently not!?
